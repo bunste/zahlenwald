06e6926868e52315787931e5e1e9ac72b99d0ea5
Stefan Bungter <stefan.bungter@pressrelations.de>
2025-09-13 08:41:51 +0200

    feat: Moved as much generic code to base-script and left only lesson specific generatorFn

--- base-script.js
+++ base-script.js
@@ -1,3 +1,8 @@
+// Setting up some state
+let ponyIndex = 0;
+let countingEnabled = true;
+let successCounter = 0;
+
 const successMessages = [
   "Super!",
   "Klasse gemacht!",
@@ -16,10 +21,94 @@ function getRandomSuccessMessage() {
   return successMessages[index];
 }
 
+const ponies = shuffle([
+  '../images/pony01.png',
+  '../images/pony02.png',
+  '../images/pony03.png',
+  '../images/pony04.png',
+  '../images/pony05.png',
+  '../images/pony06.png',
+  '../images/pony07.png',
+  '../images/pony08.png'
+]);
+const preloadedImages = [];
+$.each(ponies, function(index, src) {
+  const img = new Image();
+  img.src = src;
+  preloadedImages.push(img);
+});
+
+function showPony() {
+  const ponyImg = $('#pony');
+  ponyImg.attr('src', ponies[ponyIndex]).fadeIn();
+  ponyIndex = (ponyIndex+1) % ponies.length;
+}
+
+function renderTask(task) {
+  $('#numA').text(task.numbers[0]);
+  $('#numB').text(task.numbers[1]);
+  $('#answer').val('');
+  $('#message').removeClass('show');
+  $('#pony').fadeOut();
+
+  countingEnabled = true;
+}
+
+function checkAnswer(task) {
+  console.log(task);
+  const answerStr = $('#answer').val().trim();
+
+  if(answerStr === "") {
+    $('#message').removeClass('show');
+    return;
+  }
+
+  const userAnswer = Number(answerStr);
+
+  if(userAnswer === task.correctAnswer) {
+    // Show confetti
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.7 }
+    });
+
+    if (countingEnabled) {
+      countingEnabled = false;
+      successCounter += 1;
+      if (successCounter % 5 === 0) {
+        showPony();
+      }
+      const message = getRandomSuccessMessage();
+      $('#message').text(message).removeClass('error').addClass('success show');
+      $('#success-counter').addClass('success show').text(successCounter);
+    }
+  } else {
+    $('#message').text("Versuche es nochmal!").removeClass('success').addClass('error show');
+  }
+}
+
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
     [array[i], array[j]] = [array[j], array[i]];   // swap elements
   }
   return array;
+}
+
+function startLesson(generatorFn) {
+  let generator = generatorFn;
+  let currentTask = null;
+
+  function newTask() {
+    currentTask = generator();
+    renderTask(currentTask);
+  }
+
+  // Render first exercise
+  newTask();
+
+  $('#nextExercise').click(() => newTask());
+
+  $('#checkAnswer').click(() => checkAnswer(currentTask));
 }
--- subtraction01/subtraction01.js
+++ subtraction01/subtraction01.js
@@ -1,85 +1,14 @@
+function generateSubtraction() {
+  const a = Math.floor(Math.random() * (100 - 2 + 1)) + 2; // 2 bis 100 inkl.
+  const b = Math.floor(Math.random() * (a - 1)) + 1; // 1 <= b < a
+
+  return {
+    numbers: [a, b],
+    operator: '-',
+    correctAnswer: a - b
+  };
+}
+
 $(document).ready(function(){
-  let successCounter = 0;
-  let countingEnabled = true;
-  let ponyIndex = 0;
-
-  const ponies = shuffle([
-    '../images/pony01.png',
-    '../images/pony02.png',
-    '../images/pony03.png',
-    '../images/pony04.png',
-    '../images/pony05.png',
-    '../images/pony06.png',
-    '../images/pony07.png',
-    '../images/pony08.png'
-  ]);
-  const preloadedImages = [];
-  $.each(ponies, function(index, src) {
-    const img = new Image();
-    img.src = src;
-    preloadedImages.push(img);
-  });
-
-  function showPony() {
-    const ponyImg = $('#pony');
-    ponyImg.attr('src', ponies[ponyIndex]).fadeIn();
-    ponyIndex = (ponyIndex+1) % ponies.length;
-  }
-
-  function newExercise() {
-    const a = Math.floor(Math.random() * (100 - 2 + 1)) + 2; // 2 bis 100 inkl.
-    const b = Math.floor(Math.random() * (a - 1)) + 1; // 1 <= b < a
-
-    $('#numA').text(a);
-    $('#numB').text(b);
-    $('#answer').val('');
-    $('#message').removeClass('show');
-    $('#pony').fadeOut();
-
-    countingEnabled = true;
-  }
-
-  // Generate first exercise
-  newExercise();
-
-  // Click handlers
-
-  $('#nextExercise').click(function() {
-    newExercise();
-  });
-
-  $('#checkAnswer').click(function() {
-    const answerStr = $('#answer').val().trim();
-
-    if(answerStr === "") {
-      $('#message').removeClass('show');
-      return;
-    }
-
-    const answer = Number(answerStr);
-    const a = Number($('#numA').text());
-    const b = Number($('#numB').text());
-    
-    if(answer === a - b) {
-      // Show confetti
-      confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: { y: 0.7 }
-      });
-
-      if (countingEnabled) {
-        countingEnabled = false;
-        successCounter += 1;
-        if (successCounter % 5 === 0) {
-          showPony();
-        }
-        const message = getRandomSuccessMessage();
-        $('#message').text(message).removeClass('error').addClass('success show');
-        $('#success-counter').addClass('success show').text(successCounter);
-      }
-    } else {
-      $('#message').text("Versuche es nochmal!").removeClass('success').addClass('error show');
-    }
-  });
+  startLesson(generateSubtraction);
 });
