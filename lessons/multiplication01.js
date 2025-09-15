db403e9929a47b134a10fe7ed7fc792b9a4599f9
Stefan Bungter <stefan.bungter@pressrelations.de>
2025-09-15 09:52:43 +0200

    feat: Added new lesson for multiplication. Closes #1

---
+++ lessons/multiplication01.js
@@ -0,0 +1,15 @@
+function generateMultiplication() {
+  const a = Math.floor(Math.random() * 10) + 1;
+  const b = Math.floor(Math.random() * 10) + 1;
+
+  return {
+    numbers: [a, b],
+    operator: 'x',
+    correctAnswer: a*b
+  };
+}
+
+$(document).ready(function(){
+  $("#lessonTitle").text("Kleines Einmaleins");
+  startLesson(generateMultiplication);
+});
