1155c3d9d0392b4a85de70715936946bb01f69e3
Stefan Bungter <stefan.bungter@pressrelations.de>
2026-01-10 15:46:43 +0100

    Addition (2): Zweite Zahl entweder 1-9, 10-90 oder 100-900

--- lessons/addition02.js
+++ lessons/addition02.js
@@ -1,11 +1,21 @@
+// Zweite Zahl entweder 1-9, 10-90 oder 100-900
 function generateAddition() {
-  const a = Math.floor(Math.random() * (1000 - 2 + 1)) + 2; // 2 bis 100 inkl.
-  const b = Math.floor(Math.random() * (a - 1)) + 1; // 1 <= b < a
+
+  // Schritt 1: Zufällige Zahl zwischen 1 und 9
+  const zahl = Math.floor(Math.random() * 9) + 1;
+
+  // Schritt 2: Zufällige Auswahl, ob wir 0, 1 oder 2 Nullen anhängen
+  const nullen = Math.floor(Math.random() * 3);
+
+  // Endergebnis
+  const a = zahl * Math.pow(10, nullen);
+
+  const b = Math.floor(Math.random() * (1000 - a)) + 1;
 
   return {
-    numbers: [a-b, b],
+    numbers: [b, a],
     operator: '+',
-    correctAnswer: a
+    correctAnswer: b+a
   };
 }
 
